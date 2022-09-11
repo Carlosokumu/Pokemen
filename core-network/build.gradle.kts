e61ccefad1cae7066645b9f1ef4839eb0d41eec9
Carlos <carlosokumu254@gmail.com>
2022-09-11 13:21:16 -0700

    Refactored Code

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -41,13 +41,10 @@ dependencies {
     kapt(libs.hilt.compiler)
 
     implementation(libs.androidx.ktx)
-   // implementation("androidx.core:core-ktx:1.7.0")
-    //implementation("androidx.appcompat:appcompat:1.5.1")
-    //implementation("com.google.android.material:material:1.6.1")
     testImplementation(libs.junit)
-    //testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation(libs.androidx.junit)
-    //androidTestImplementation("androidx.test.ext:junit:1.1.3")
+
     androidTestImplementation(libs.androidx.espresso)
-    //androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    
 }
