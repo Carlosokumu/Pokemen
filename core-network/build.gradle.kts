c1e5f4ebf825ad979c8a9436a982797729e4db9d
Carlos <carlosokumu254@gmail.com>
2022-09-11 13:19:37 -0700

    Load libraries from TOML file

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -1,14 +1,17 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(libs.plugins.android.library.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+    id(libs.plugins.kotlin.kapt.get().pluginId)
 }
 
 android {
-    compileSdk = 32
+    compileSdk = Configuration.compileSdk
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 32
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -34,10 +37,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.androidx.ktx)
+   // implementation("androidx.core:core-ktx:1.7.0")
+    //implementation("androidx.appcompat:appcompat:1.5.1")
+    //implementation("com.google.android.material:material:1.6.1")
+    testImplementation(libs.junit)
+    //testImplementation("junit:junit:4.13.2")
+    androidTestImplementation(libs.androidx.junit)
+    //androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation(libs.androidx.espresso)
+    //androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
