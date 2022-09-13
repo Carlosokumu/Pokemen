5094a8a2a2555b5f093d26decfe1965bba49e1e5
Carlos <carlosokumu254@gmail.com>
2022-09-13 04:26:18 -0700

    Removed Errors

--- core-network/src/main/java/com/carlos/network/network/ApiClient.kt
+++ core-network/src/main/java/com/carlos/network/network/ApiClient.kt
@@ -1,10 +1,19 @@
 package com.carlos.network.network
 
+import com.carlos.network.models.PokemonResponse
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
-class ApiClient @Inject constructor(private val pokemenService: PokemenService) {
+class ApiClient @Inject constructor(private val ioDispatcher: CoroutineDispatcher,val pokemenService: PokemenService) {
 
-    suspend fun fetchPokemonList(){
-
+    suspend fun fetchPokemonList(page: Int): ApiCallResult<PokemonResponse> = safeApiCall(ioDispatcher) {
+        return@safeApiCall pokemenService.fetchPokemonList(limit = LIMIT, offset = page)
     }
+
+
+    companion object {
+        const val LIMIT = 20
+    }
+
+
 }
