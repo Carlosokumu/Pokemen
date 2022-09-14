2d64649e7625e21282dbb76a931a8706e0c8fe6a
Carlos <carlosokumu254@gmail.com>
2022-09-14 11:45:13 -0700

    Implemented function

--- app/src/main/java/com/carlos/pokemen/data/repository/MainRepositoryImp.kt
+++ app/src/main/java/com/carlos/pokemen/data/repository/MainRepositoryImp.kt
@@ -1,5 +1,29 @@
 package com.carlos.pokemen.data.repository
 
-class MainRepositoryImp() {
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.carlos.network.models.Pokemon
+import com.carlos.network.network.ApiClient
+import com.carlos.network.network.safeApiCall
+import com.carlos.pokemen.paging.PokemonDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class MainRepositoryImp @Inject constructor(private val apiClient: ApiClient): MainRepository {
+
+    override suspend fun fetchPokemonList(): Flow<PagingData<Pokemon>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 20),
+            pagingSourceFactory = {
+                PokemonDataSource(apiClient = apiClient)
+            }
+        ).flow
+    }
+
+
+
+
 
 }
