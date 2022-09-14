97713964a2e3b81a690c5ed356902c0468674f6e
Carlos <carlosokumu254@gmail.com>
2022-09-14 11:54:03 -0700

    Added binding information for okhttp

--- core-network/src/main/java/com/carlos/network/di/NetworkModule.kt
+++ core-network/src/main/java/com/carlos/network/di/NetworkModule.kt
@@ -10,11 +10,22 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        return   OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
 
     @Provides
     @Singleton
