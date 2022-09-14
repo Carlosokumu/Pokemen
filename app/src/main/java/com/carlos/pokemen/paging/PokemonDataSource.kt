b6d85e1bb3f8b5952a9d297e9fd7f58ca225281c
Carlos <carlosokumu254@gmail.com>
2022-09-14 11:48:56 -0700

    Finished creation of paging datasource

---
+++ app/src/main/java/com/carlos/pokemen/paging/PokemonDataSource.kt
@@ -0,0 +1,33 @@
+package com.carlos.pokemen.paging
+
+import androidx.paging.PageKeyedDataSource
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.carlos.network.models.Pokemon
+import com.carlos.network.models.PokemonResponse
+import com.carlos.network.network.ApiCallResult
+import com.carlos.network.network.ApiClient
+import java.io.IOException
+
+class PokemonDataSource(private val apiClient: ApiClient): PagingSource<Int, Pokemon>() {
+    override fun getRefreshKey(state: PagingState<Int, Pokemon>): Int? {
+          return  state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Pokemon> {
+        return try {
+            val nextPage = params.key ?: 1
+            val pokemonList = apiClient.fetchPokemonList(page = nextPage)
+            LoadResult.Page(
+                data = pokemonList.results,
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (pokemonList.results.isEmpty()) null else  1
+            )
+
+        } catch (e: IOException) {
+            return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+}
