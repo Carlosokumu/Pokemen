d4488bef0cd25a4a0e72a825b246b6261269ecf2
Carlos <carlosokumu254@gmail.com>
2022-09-14 11:44:47 -0700

    changed  function return type

--- app/src/main/java/com/carlos/pokemen/data/repository/MainRepository.kt
+++ app/src/main/java/com/carlos/pokemen/data/repository/MainRepository.kt
@@ -1,6 +1,12 @@
 package com.carlos.pokemen.data.repository
 
+import androidx.paging.PagingData
+import com.carlos.network.models.Pokemon
+import com.carlos.network.models.PokemonResponse
+import com.carlos.network.network.ApiCallResult
+import kotlinx.coroutines.flow.Flow
+
 interface MainRepository {
 
-    suspend fun  fetchPokemonList(page: Int)
+    suspend fun  fetchPokemonList(): Flow<PagingData<Pokemon>>
 }
