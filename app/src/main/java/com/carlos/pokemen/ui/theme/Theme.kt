9dc1ab11ea95db66c89b8da8637f3281325b3023
Carlos <carlosokumu254@gmail.com>
2022-09-23 22:05:28 -0700

    Changed Status Bar Color

--- app/src/main/java/com/carlos/pokemen/ui/theme/Theme.kt
+++ app/src/main/java/com/carlos/pokemen/ui/theme/Theme.kt
@@ -1,34 +1,50 @@
-package com.example.pokemen.ui.theme
+package com.carlos.pokemen.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red700,
+    primaryVariant = Purple200,
+    secondaryVariant = Purple200,
+    secondary = Teal200,
+    onBackground = Color.Black,
+    surface = Color.Black,
+    onSurface = Color.Black,
+
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+
+private val LightColorPalette = lightColors(
+//   primary = Purple200,
+//    primaryVariant = Purple200,
+//    secondary = Teal200,
+    background = Whitemain,
+    surface = Whitemain ,
+//    surface = Color.White,
+//   onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+    primary = Whitemain,
+    primaryVariant = Red900,
+    //onPrimary = Color.White,
+    secondary = Whitemain,
+    secondaryVariant = Red900,
+    //onSecondary = Red700,
+    error = Red800
+
+
+
 )
 
 @Composable
-fun PokedexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun PokedexTheme(darkTheme: Boolean, content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
