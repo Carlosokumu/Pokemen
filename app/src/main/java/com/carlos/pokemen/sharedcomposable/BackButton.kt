41ab7c162b8bfd6f921539780dfe13c94a90fca8
Carlos <carlosokumu254@gmail.com>
2022-09-26 06:16:28 -0700

    Created back Button

---
+++ app/src/main/java/com/carlos/pokemen/sharedcomposable/BackButton.kt
@@ -0,0 +1,28 @@
+package com.carlos.pokemen.sharedcomposable
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.carlos.pokemen.ui.theme.statusColor
+
+
+@Composable
+fun BackButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    FloatingActionButton(
+        modifier = modifier.size(42.dp),
+        backgroundColor = statusColor,
+        contentColor = Color.White,
+        onClick = { onClick() }) {
+        Icon(
+            imageVector = Icons.Rounded.ArrowBack,
+            contentDescription = "back icon",
+        )
+    }
+}
