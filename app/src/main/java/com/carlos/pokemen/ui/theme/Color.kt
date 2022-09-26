1542da7b9a11113fdd9665dae5d3f0ea86384535
Carlos <carlosokumu254@gmail.com>
2022-09-26 06:17:43 -0700

    More colors

--- app/src/main/java/com/carlos/pokemen/ui/theme/Color.kt
+++ app/src/main/java/com/carlos/pokemen/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.pokemen.ui.theme
+package com.carlos.pokemen.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -6,3 +6,35 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Red700 = Color(0xffdd0d3c)
+
+
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+
+
+
+
+val colorPrimary = Color(0xFF6200EEE)
+val mainColor = Color(0xff0d47a1)
+val statusColor = Color(0xff0D47A1)
+val Whitemain = Color(0xfffafafa)
+
+
+val white = Color(0xffffffff)
+val black = Color(0xff000000)
+val paleWhite = Color(0xfff3f7f9)
+val paleBlack = Color(0xff222325)
+val blue = Color(89, 155, 254)
+val ghostWhite = Color(245, 246, 248)
+val platinum = Color(200, 202, 204)
+val lightSilver = Color(171, 172, 173)
+val gold = Color(238, 141, 60, 255)
+val navajoWhite = Color(254, 228, 162)
+val water = Color(220, 237, 255)
+val lightBlue = Color(229, 240, 252)
+
+val seashell = Color(253, 246, 235)
+val aliceBlue = Color(237, 246, 255)
+val cultured = Color(244, 246, 248)
+val azureishWhite = Color(220, 237, 247)
