b707ea8daf67aaf506bc3efc24fb6b38bc1846bc
Carlos <carlosokumu254@gmail.com>
2022-09-26 12:29:09 -0700

    Creating pokemon info

--- core-network/src/main/java/com/carlos/network/models/PokemonInfo.kt
+++ core-network/src/main/java/com/carlos/network/models/PokemonInfo.kt
@@ -1,4 +1,32 @@
 package com.carlos.network.models
 
-class PokemonInfo {
+import kotlin.random.Random
+
+class PokemonInfo(
+    val name: String,
+    val hp: Int = Random.nextInt(maxHp),
+    val attack: Int = Random.nextInt(maxAttack),
+    val defense: Int = Random.nextInt(maxDefense),
+    val speed: Int = Random.nextInt(maxSpeed),
+    val exp: Int = Random.nextInt(maxExp),
+    val height: Int,
+     val weight: Int
+) {
+
+
+    fun getWeightString(): String = String.format("%.1f KG", weight.toFloat() / 10)
+    fun getHeightString(): String = String.format("%.1f M", height.toFloat() / 10)
+    fun getHpString(): String = "$hp/$maxHp"
+    fun getAttackString(): String = "$attack/$maxAttack"
+    fun getDefenseString(): String = "$defense/$maxDefense"
+    fun getSpeedString(): String = "$speed/$maxSpeed"
+    fun getExpString(): String = "$exp/$maxExp"
+
+    companion object {
+        const val maxHp = 300
+        const val maxAttack = 300
+        const val maxDefense = 300
+        const val maxSpeed = 300
+        const val maxExp = 1000
+    }
 }
