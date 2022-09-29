fe2b49207127546458cde5e857beb94ee22c332e
Carlos <carlosokumu254@gmail.com>
2022-09-29 03:53:48 -0700

    Added fonts

--- app/src/main/java/com/carlos/pokemen/ui/theme/Type.kt
+++ app/src/main/java/com/carlos/pokemen/ui/theme/Type.kt
@@ -1,10 +1,21 @@
-package com.example.pokemen.ui.theme
+package com.carlos.pokemen.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.pokedex.R
+
+
+val poppins = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppin_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -12,7 +23,7 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
@@ -25,4 +36,35 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+    h1 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Light,
+        fontSize = 15.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.Light,
+        fontSize = 20.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = poppins,
+        fontWeight = FontWeight.ExtraLight,
+    )
+
+
 )
