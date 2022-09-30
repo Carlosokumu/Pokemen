a79a8473fb85fd8f82e35076c0944d029a532cd0
Carlos <carlosokumu254@gmail.com>
2022-09-30 12:27:38 -0700

    Code clean-up & import optimization

--- app/src/main/java/com/carlos/pokemen/screens/PokemonListView.kt
+++ app/src/main/java/com/carlos/pokemen/screens/PokemonListView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -22,10 +23,13 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.palette.graphics.Palette
 import coil.ImageLoader
 import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.request.SuccessResult
 import coil.size.Size
+import com.carlos.network.models.Pokemon
 import com.carlos.pokemen.getImageUrl
 import com.carlos.pokemen.navigation.MainActions
 import com.carlos.pokemen.viewmodels.HomeViewModel
@@ -35,7 +39,7 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PokemonListView(homeViewModel: HomeViewModel, state: LazyGridState, mainActions: MainActions) {
-    var color: Int? = null
+
 
     val pokemons = homeViewModel.pokemon.value.collectAsLazyPagingItems()
     LaunchedEffect(key1 = 10) {
@@ -50,138 +54,109 @@ fun PokemonListView(homeViewModel: HomeViewModel, state: LazyGridState, mainActi
     ) {
 
         items(pokemons.itemCount) { index ->
-            Card(
-                modifier = Modifier.padding(4.dp),
-                elevation = 10.dp,
-                onClick = {
-                    mainActions.gotoDetailsScreen(pokemons[index]!!.name,index)
-                },
-                backgroundColor =  Color.White
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .wrapContentHeight()
-                            .fillMaxWidth()
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 6.dp, start = 6.dp, end = 6.dp),
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            verticalAlignment = Alignment.Top,
-                        ) {
-                            Text(
-                                text = pokemons[index]!!.name,
-                                color = Color.Blue,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = index.toString(),
-                                color = Color.Black,
-                                textAlign = TextAlign.Center,
-                            )
-                        }
-                        val context = LocalContext.current
+            val context = LocalContext.current
+            val loader = ImageLoader(context)
 
-                        val imageLoader = ImageLoader(context)
-
-
-                        val painter = rememberAsyncImagePainter(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(getImageUrl(url = pokemons[index]!!.url))
-                                .allowHardware(false)
-                                .size(Size.ORIGINAL) // Set the target size to load the image at.
-                                .build()
-                        )
-                        if (painter.state is AsyncImagePainter.State.Success) {
-                            LaunchedEffect(key1 = painter) {
-                                launch {
-                                    val result =
-                                        (imageLoader.execute(painter.request) as SuccessResult).drawable
-                                    val bitmap = (result as BitmapDrawable).bitmap
-                                    val vibrant = Palette.from(bitmap)
-                                        .generate()
-                                    color = vibrant.dominantSwatch?.rgb
-
-
-                                    Log.d("COLORGEN", color.toString())
-                                }
-
-                            }
-
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(getImageUrl(url = pokemons[index]!!.url))
+                    .allowHardware(false)
+                    .size(Size.ORIGINAL) // Set the target size to load the image at.
+                    .build()
+            )
+            if (painter.state is AsyncImagePainter.State.Success) {
+                var color = 123342
+                LaunchedEffect(key1 = painter) {
+                    launch {
+                        val result =
+                            (loader.execute(painter.request) as SuccessResult).drawable
+                        val bitmap = (result as BitmapDrawable).bitmap
+                        val vibrant = Palette.from(bitmap)
+                            .generate()
+                        val dominantSwatch = vibrant.dominantSwatch?.rgb
+                        if (dominantSwatch != null) {
+                            homeViewModel.setColor(dominantSwatch)
+                            color = dominantSwatch
                         }
 
-
+                        pokemons[index]!!.color = homeViewModel.color.value
 
                     }
+
                 }
+
+                PokemonCard(
+                    cardBackground = Color( pokemons[index]!!.color),
+                    mainActions = mainActions,
+                    pokemon = pokemons[index]!!,
+                    index = index
+                )
+
+
             }
         }
     }
 }
-
-
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun PokeMonView(color: Color?,homeViewModel: HomeViewModel) {
-    val pokemons = homeViewModel.pokemon.value.collectAsLazyPagingItems()
-    LaunchedEffect(key1 = 10) {
-        homeViewModel.getPokemonList()
-    }
-
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(8.dp),
-
+fun PokemonCard(cardBackground: Color,mainActions: MainActions,pokemon: Pokemon,index: Int){
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = 10.dp,
+        onClick = {
+            mainActions.gotoDetailsScreen(pokemon.name, index)
+        },
+        backgroundColor = cardBackground
     ) {
-
-        items(pokemons.itemCount) { index ->
-            Card(
-                modifier = Modifier.padding(4.dp),
-                elevation = 10.dp,
-                onClick = {
-                    //mainActions.gotoDetailsScreen()
-                },
-                backgroundColor = color ?: Color.White
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
             ) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalArrangement = Arrangement.Center
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 6.dp, start = 6.dp, end = 6.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.Top,
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .wrapContentHeight()
-                            .fillMaxWidth()
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 6.dp, start = 6.dp, end = 6.dp),
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            verticalAlignment = Alignment.Top,
-                        ) {
-                            Text(
-                                text = pokemons[index]!!.name,
-                                color = Color.Blue,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = index.toString(),
-                                color = Color.Black,
-                                textAlign = TextAlign.Center,
-                            )
-                        }
+                    Text(
+                        text = pokemon.name,
+                        color = Color.Blue,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = index.toString(),
+                        color = Color.Black,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                SubcomposeAsyncImage(
+                    model = getImageUrl(pokemon.url),
+                    contentDescription = "null",
+                    alignment = Alignment.Center
+                ) {
+                    val state = painter.state
+                    if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center), color = cardBackground)
+                    } else {
+                        SubcomposeAsyncImageContent()
                     }
                 }
+
+
             }
         }
     }
 }
+
+
+
+
 
 
 
