e258b36161d380773d8e2151db51b80975847ddd
Carlos <carlosokumu254@gmail.com>
2022-09-30 12:14:19 -0700

    Code clean-up & import optimization

--- app/src/main/java/com/carlos/pokemen/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/carlos/pokemen/viewmodels/HomeViewModel.kt
@@ -2,10 +2,6 @@ package com.carlos.pokemen.viewmodels
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
@@ -14,17 +10,17 @@ import com.carlos.pokemen.data.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
+class HomeViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
 
     private var _pokemon = mutableStateOf<Flow<PagingData<Pokemon>>>(emptyFlow())
     val pokemon: State<Flow<PagingData<Pokemon>>> = _pokemon
 
-
+    private var _colorValue = mutableStateOf(-7839624)
+    val color = _colorValue
 
 
     fun  getPokemonList() = viewModelScope.launch {
@@ -32,7 +28,17 @@ class MainViewModel @Inject constructor(private val mainRepository: MainReposito
     }
 
 
+    fun setColor(color: Int){
+        _colorValue.value = color
+    }
+
+
 
 
 
 }
+
+
+
+
+
