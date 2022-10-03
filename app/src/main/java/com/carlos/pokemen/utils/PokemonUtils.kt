5be345d2013144d84c5ea78d96ccdf7061bc1131
Carlos <carlosokumu254@gmail.com>
2022-10-03 13:23:25 -0700

    Added More Helper functions

--- app/src/main/java/com/carlos/pokemen/utils/PokemonUtils.kt
+++ app/src/main/java/com/carlos/pokemen/utils/PokemonUtils.kt
@@ -35,9 +35,41 @@ object PokemonUtils {
         return (stat/100).toFloat()
     }
 
+    fun getTypeColor(type: String): Color {
+        return when (type) {
+            "fighting" -> fighting
+            "flying" -> flying
+            "poison" -> poison
+            "ground" -> ground
+            "rock" -> rock
+            "bug" -> bug
+            "ghost" -> ghost
+            "steel" -> steel
+            "fire" -> fire
+            "water" -> water
+            "grass" -> grass
+            "electric" -> electric
+            "psychic" -> psychic
+            "ice" ->  ice
+            "dragon" -> dragon
+            "fairy" -> fairy
+            "dark" -> Color.DarkGray
+            else -> Color.LightGray
+        }
+    }
+
     fun getImageUrl(url: String): String {
         val index = url.split("/".toRegex()).dropLast(1).last()
         return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png"
+    }
+
+
+    fun getIndex(index: Int): String{
+        return when(index){
+            in 0..9 -> "#00".plus(index.toString())
+            in 10..99 -> "#0".plus(index.toString())
+            else -> "#".plus(index.toString())
+        }
     }
 
 }
