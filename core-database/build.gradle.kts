07e83f3b2adb0d98b37fdb74da1cf7c25f745ea8
Carlos <carlosokumu254@gmail.com>
2022-10-18 00:13:24 -0700

    CONFIGURATIONS

--- core-database/build.gradle.kts
+++ core-database/build.gradle.kts
@@ -1,15 +1,17 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
-    id(libs.plugins.android.library.get().pluginId)
-    id(libs.plugins.kotlin.android.get().pluginId)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     id(libs.plugins.kotlin.kapt.get().pluginId)
 }
 
 android {
-    compileSdk = Configuration.compileSdk
+    compileSdk = 32
 
     defaultConfig {
-        minSdk = Configuration.minSdk
-        targetSdk = Configuration.targetSdk
+        minSdk = 21
+        targetSdk = 32
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -35,27 +37,19 @@ android {
 
 dependencies {
 
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    implementation(libs.coroutines)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
-
-    implementation(libs.androidx.ktx)
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.junit)
-
-    androidTestImplementation(libs.androidx.espresso)
-
-    //Network
-    implementation(libs.retrofit)
-    implementation(libs.okhttp.interceptor)
-    implementation(libs.gson)
-    implementation(libs.okhttp)
-
-    //Room
     implementation(libs.androidx.room)
     annotationProcessor(libs.androidx.room.compiler)
+
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
 
     implementation(libs.androidx.paging)
     implementation(libs.coroutines)
