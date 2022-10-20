0620a689352f21e94a3ba1ff0b4d4f73a5fb9973
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:23:48 -0700

    Upgraded sdk version

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,7 +1,7 @@
 
 object  Configuration{
-    const val compileSdk = 32
-    const val targetSdk = 32
+    const val compileSdk = 33
+    const val targetSdk = 33
     const val minSdk = 23
     const val versionCode = 1
     const val versionName = "1.0"
