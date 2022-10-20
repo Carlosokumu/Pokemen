7e92210877fe2d4da1b494a224692626e19ec0e8
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:34:41 -0700

    Injected Repository Module

--- app/src/main/java/com/carlos/pokemen/di/RepositoryModule.kt
+++ app/src/main/java/com/carlos/pokemen/di/RepositoryModule.kt
@@ -1,16 +1,16 @@
-package com.carlos.pokemen.di
-
-import com.carlos.pokemen.data.repository.MainRepository
-import com.carlos.pokemen.data.repository.MainRepositoryImp
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-
-@Module
-@InstallIn(SingletonComponent::class)
-interface DataModule {
-
-    @Binds
-    fun bindsMainRepository(mainRepositoryImp: MainRepositoryImp): MainRepository
-}
+//package com.carlos.pokemen.di
+//
+//import com.carlos.pokemen.data.repository.MainRepository
+//import com.carlos.pokemen.data.repository.MainRepositoryImp
+//import dagger.Binds
+//import dagger.Module
+//import dagger.hilt.InstallIn
+//import dagger.hilt.components.SingletonComponent
+//
+//@Module
+//@InstallIn(SingletonComponent::class)
+//interface DataModule {
+//
+//    @Binds
+//    fun bindsMainRepository(mainRepositoryImp: MainRepositoryImp): MainRepository
+//}
