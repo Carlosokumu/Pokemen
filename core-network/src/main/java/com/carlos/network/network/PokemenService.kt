8afda631acf81e9091edbf855638488c3bc8d793
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:26:38 -0700

    Optimize imports

--- core-network/src/main/java/com/carlos/network/network/PokemenService.kt
+++ core-network/src/main/java/com/carlos/network/network/PokemenService.kt
@@ -1,9 +1,7 @@
 package com.carlos.network.network
 
-import com.carlos.network.models.PokemonInfo
+import com.carlos.model.PokemonInfo
 import com.carlos.network.models.PokemonResponse
-import com.carlos.network.network.ApiCallResult
-import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
