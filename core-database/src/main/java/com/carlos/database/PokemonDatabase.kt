41f571db73d274ce96adbd76c6cd48e21a10b276
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:33:24 -0700

    Changing the the Db-version,included DAOS

--- core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
+++ core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
@@ -2,16 +2,20 @@ package com.carlos.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.carlos.database.dao.PokemonDao
+import com.carlos.database.dao.RemotePageDao
 import com.carlos.database.entity.PokemonEntity
+import com.carlos.database.entity.RemoteKey
 
 
 @Database(
-    entities = [PokemonEntity::class],
-    version = 2,
+    entities = [PokemonEntity::class,RemoteKey::class],
+    version = 1,
     exportSchema = true
 )
 
 abstract class PokemonDatabase : RoomDatabase() {
-
+    abstract fun pokemonDao(): PokemonDao
+    abstract fun remoteKeyDao(): RemotePageDao
 
 }
