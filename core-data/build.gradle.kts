dda1572ab5f13662c5a992e46c9f4133d6a2f457
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:22:56 -0700

    Upgrading room dependencies

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -38,16 +38,18 @@ dependencies {
 
     implementation(project(":core-model"))
     implementation(project(":core-network"))
+    implementation(project(":core-database"))
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+   // androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
     implementation(libs.androidx.room)
-    annotationProcessor(libs.androidx.room.compiler)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.extensions)
 
 
     implementation(libs.hilt.android)
