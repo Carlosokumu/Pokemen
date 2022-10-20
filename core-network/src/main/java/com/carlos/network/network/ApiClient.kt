9686e811bdd10fa33469f4c0372cebad2c72246c
Carlos <carlosokumu254@gmail.com>
2022-10-20 04:20:53 -0700

    Removed explicit return type

--- core-network/src/main/java/com/carlos/network/network/ApiClient.kt
+++ core-network/src/main/java/com/carlos/network/network/ApiClient.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 
 class ApiClient @Inject constructor(val dispatcher: CoroutineDispatcher,val pokemenService: PokemenService) {
 
-    suspend fun fetchPokemonList(page: Int): PokemonResponse = pokemenService.fetchPokemonList(offset = page)
+    suspend fun fetchPokemonList(page: Int) = pokemenService.fetchPokemonList(offset = page)
 
     suspend fun fetchPokemonInfo(name: String) = safeApiCall(dispatcher){
         return@safeApiCall pokemenService.fetchPokemonInfo(name = name)
