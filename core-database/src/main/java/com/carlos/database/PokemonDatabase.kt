956aed625a505a1bfa1d4720a53f267f3301dfb1
Carlos <carlosokumu254@gmail.com>
2022-10-24 12:59:30 -0700

    [UPDATE]Removed unused RemoteKey Entity and the Daos

--- core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
+++ core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
@@ -3,19 +3,15 @@ package com.carlos.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.carlos.database.dao.PokemonDao
-import com.carlos.database.dao.RemotePageDao
 import com.carlos.database.entity.PokemonEntity
-import com.carlos.database.entity.RemoteKey
 
 
 @Database(
-    entities = [PokemonEntity::class,RemoteKey::class],
+    entities = [PokemonEntity::class],
     version = 1,
     exportSchema = true
 )
 
 abstract class PokemonDatabase : RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
-    abstract fun remoteKeyDao(): RemotePageDao
-
 }
