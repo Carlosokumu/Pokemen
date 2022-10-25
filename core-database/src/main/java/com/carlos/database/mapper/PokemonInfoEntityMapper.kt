79cf44424d4bb3143d6ccdb67d57d1159fdd3842
Carlos <carlosokumu254@gmail.com>
2022-10-24 23:40:04 -0700

    [UPDATE]Implemented PokemonInfo Mapper

--- core-database/src/main/java/com/carlos/database/mapper/PokemonInfoEntityMapper.kt
+++ core-database/src/main/java/com/carlos/database/mapper/PokemonInfoEntityMapper.kt
@@ -1,6 +1,18 @@
 package com.carlos.database.mapper
 
+import com.carlos.database.entity.PokemonInfoEntity
+import com.carlos.model.PokemonInfo
 
 
-object PokemonInfoEntityMapper {
+object PokemonInfoEntityMapper :EntityMapper<PokemonInfo,PokemonInfoEntity>{
+
+
+
+    override fun asEntity(domain: PokemonInfo): PokemonInfoEntity {
+
+    }
+
+    override fun asDomain(entity: PokemonInfoEntity): PokemonInfo {
+
+    }
 }
