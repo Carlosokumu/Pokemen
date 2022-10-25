c5ed4de6c19c7b3f7ac911069ff49b38e2dde34e
Carlos <carlosokumu254@gmail.com>
2022-10-24 22:18:15 -0700

    [UPDATE]A little housekeeping

--- app/src/main/java/com/carlos/pokemen/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/carlos/pokemen/viewmodels/HomeViewModel.kt
@@ -5,33 +5,46 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
-import com.carlos.network.models.Pokemon
-import com.carlos.pokemen.data.repository.MainRepository
+import androidx.paging.cachedIn
+import com.carlos.data.MainRepository
+import com.carlos.database.entity.PokemonEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
 
-    private var _pokemon = mutableStateOf<Flow<PagingData<Pokemon>>>(emptyFlow())
-    val pokemon: State<Flow<PagingData<Pokemon>>> = _pokemon
+    private var _pokemon = mutableStateOf<Flow<PagingData<PokemonEntity>>>(emptyFlow())
+    val pokemon: State<Flow<PagingData<PokemonEntity>>> = _pokemon
 
     private var _colorValue = mutableStateOf(-7839624)
     val color = _colorValue
 
 
-    fun  getPokemonList() = viewModelScope.launch {
-        _pokemon.value = mainRepository.fetchPokemonList()
+
+    init {
+        getPokemonList()
     }
+
+
+
+    private fun  getPokemonList() = viewModelScope.launch {
+        _pokemon.value = mainRepository.fetchPokemonListTwo()
+    }
+
+
+
 
 
     fun setColor(color: Int){
         _colorValue.value = color
     }
-
 
 
 
