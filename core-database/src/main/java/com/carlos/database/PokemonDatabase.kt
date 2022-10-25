2f2bbbde0dfca38ff0359e2eb17c8c208effd797
Carlos <carlosokumu254@gmail.com>
2022-10-25 00:07:40 -0700

    [UPDATE]Changed the database version,added pokemonInfo Dao

--- core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
+++ core-database/src/main/java/com/carlos/database/PokemonDatabase.kt
@@ -3,15 +3,18 @@ package com.carlos.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.carlos.database.dao.PokemonDao
+import com.carlos.database.dao.PokemonInfoDao
 import com.carlos.database.entity.PokemonEntity
+import com.carlos.database.entity.PokemonInfoEntity
 
 
 @Database(
-    entities = [PokemonEntity::class],
-    version = 1,
+    entities = [PokemonEntity::class,PokemonInfoEntity::class],
+    version = 2,
     exportSchema = true
 )
 
 abstract class PokemonDatabase : RoomDatabase() {
     abstract fun pokemonDao(): PokemonDao
+    abstract fun pokemonInfoDao(): PokemonInfoDao
 }
