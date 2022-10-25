3219b1bb2033d7170d062c62c7ab25eb71dd8502
Carlos <carlosokumu254@gmail.com>
2022-10-24 23:52:56 -0700

    [UPDATE]Implemented the Mapper

--- core-database/src/main/java/com/carlos/database/mapper/PokemonInfoEntityMapper.kt
+++ core-database/src/main/java/com/carlos/database/mapper/PokemonInfoEntityMapper.kt
@@ -4,15 +4,49 @@ import com.carlos.database.entity.PokemonInfoEntity
 import com.carlos.model.PokemonInfo
 
 
-object PokemonInfoEntityMapper :EntityMapper<PokemonInfo,PokemonInfoEntity>{
-
+object PokemonInfoEntityMapper : EntityMapper<PokemonInfo, PokemonInfoEntity> {
 
 
     override fun asEntity(domain: PokemonInfo): PokemonInfoEntity {
 
+        return PokemonInfoEntity(
+            name = domain.name,
+            id = domain.id,
+            hp = domain.HP,
+            attack = domain.ATK,
+            defence = domain.DEF,
+            speed = domain.SPD,
+            exp = domain.exp,
+            height = domain.height,
+            species = domain.species,
+            weight = domain.weight,
+            stats = domain.stats,
+            types = domain.types
+        )
     }
 
     override fun asDomain(entity: PokemonInfoEntity): PokemonInfo {
-
+       return  PokemonInfo(
+           name = entity.name,
+           id= entity.id,
+           HP = entity.hp,
+           ATK = entity.attack,
+           DEF = entity.defence,
+           SPD = entity.speed,
+           exp = entity.exp,
+           height = entity.height,
+           species = entity.species,
+           weight = entity.weight,
+           stats = entity.stats,
+           types = entity.types
+       )
     }
+
+}
+fun PokemonInfo.asEntity(): PokemonInfoEntity {
+    return PokemonInfoEntityMapper.asEntity(this)
+}
+
+fun PokemonInfoEntity.asDomain(): PokemonInfo {
+    return PokemonInfoEntityMapper.asDomain(this)
 }
