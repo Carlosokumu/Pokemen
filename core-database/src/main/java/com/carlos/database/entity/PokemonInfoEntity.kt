3d9306f82e1e578dea6811c969a87c3ca41fc20f
Carlos <carlosokumu254@gmail.com>
2022-10-29 05:40:11 -0700

    [UPDATE]Added TypeConverters to the entity

--- core-database/src/main/java/com/carlos/database/entity/PokemonInfoEntity.kt
+++ core-database/src/main/java/com/carlos/database/entity/PokemonInfoEntity.kt
@@ -1,12 +1,23 @@
 package com.carlos.database.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import com.carlos.database.converters.SpecieConvertor
+import com.carlos.database.converters.StatConvertor
+import com.carlos.database.converters.TypeResponseConvertor
 import com.carlos.model.Specie
 import com.carlos.model.Stats
 import com.carlos.model.TypeResponse
 
+
+@Entity
+@TypeConverters(value = [TypeResponseConvertor::class,StatConvertor::class,SpecieConvertor::class])
 data class PokemonInfoEntity(
-    val name: String,
+    @PrimaryKey
     val  id: Int,
+    val name: String,
     val hp: Int,
     val attack: Int,
     val defence: Int,
