25d61722e4fa4ea1670c9082ce390da5de6f0e40
Carlos <carlosokumu254@gmail.com>
2022-11-17 13:30:56 -0500

    [ADD]Added MainCoroutinesRule

---
+++ core-data/src/test/java/com/carlos/data/MainCoroutineRule.kt
@@ -0,0 +1,23 @@
+package com.carlos.data
+
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MainCoroutinesRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    val testScope = TestScope(testDispatcher)
+
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
