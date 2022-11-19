76d00327c807ac60efa5b6a2a70f9a98ba99e267
Carlos <carlosokumu254@gmail.com>
2022-11-19 07:21:33 -0500

    [UPDATE]color field mapping

--- core-database/src/main/java/com/carlos/database/mapper/PokemonEntityMapper.kt
+++ core-database/src/main/java/com/carlos/database/mapper/PokemonEntityMapper.kt
@@ -7,13 +7,13 @@ object PokemonEntityMapper : EntityMapper<List<Pokemon>, List<PokemonEntity>> {
 
     override fun asEntity(domain: List<Pokemon>): List<PokemonEntity> {
         return domain.map {
-            PokemonEntity(page = it.page, name = it.name, url = it.url)
+            PokemonEntity(page = it.page, name = it.name, url = it.url,color = it.color)
         }
     }
 
     override fun asDomain(entity: List<PokemonEntity>): List<Pokemon> {
         return entity.map {
-            Pokemon(page = it.page, url = it.url, name = it.name)
+            Pokemon(page = it.page, url = it.url, name = it.name, color = it.color)
         }
     }
 
@@ -24,5 +24,5 @@ fun List<Pokemon>.asEntity(): List<PokemonEntity> {
 }
 
 fun PokemonEntity.asDomain(): Pokemon {
-    return Pokemon(page = this.page,url = this.url,name = this.name)
+    return Pokemon(page = this.page,url = this.url,name = this.name,color = this.color)
 }
