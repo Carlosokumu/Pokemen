6a913f90bbeaea7db5961d79a4fefe1e0b2a90c3
Carlos <carlosokumu254@gmail.com>
2023-02-06 13:21:45 -0500

    [UPDATE]Removed Preview annotation

--- app/src/main/java/com/carlos/pokemen/sharedcomposable/BackButton.kt
+++ app/src/main/java/com/carlos/pokemen/sharedcomposable/BackButton.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.carlos.pokemen.ui.theme.statusColor
 
