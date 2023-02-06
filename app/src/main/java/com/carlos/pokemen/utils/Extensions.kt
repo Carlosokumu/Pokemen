3ec2655e8a11756c789a0999b0624381b67061c0
Carlos <carlosokumu254@gmail.com>
2023-02-06 13:23:50 -0500

    [UPDATE]Optimize imports

--- app/src/main/java/com/carlos/pokemen/utils/Extensions.kt
+++ app/src/main/java/com/carlos/pokemen/utils/Extensions.kt
@@ -1,5 +1,10 @@
 package com.carlos.pokemen.utils
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.material.Card
+
+
+fun LazyGridState.isScrolledToEnd() = layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+
 
